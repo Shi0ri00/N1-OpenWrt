f61cdea820ab75ea7c41cc5f2c63e1fc96e064c7
Shiori <106437447+Shi0ri00@users.noreply.github.com>
2024-08-21 21:46:18 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -6,26 +6,6 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 # Default IP
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 
-function merge_package(){
-    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
-    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
-    git clone --depth=1 --single-branch $1
-    mv $2 package/custom/
-    rm -rf $repo
-}
-function drop_package(){
-    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
-}
-function merge_feed(){
-    if [ ! -d "feed/$1" ]; then
-        echo >> feeds.conf.default
-        echo "src-git $1 $2" >> feeds.conf.default
-    fi
-    ./scripts/feeds update $1
-    ./scripts/feeds install -a -p $1
-}
-
 rm -rf package/custom; mkdir package/custom
 
 # Remove packages
@@ -33,8 +13,8 @@ rm -rf feeds/luci/applications/luci-app-passwall
 
 # Add packages
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
-merge_package https://github.com/morytyann/OpenWrt-mihomo Openwrt-mihomo
+git clone --depth=1 https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
+git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo Openwrt-mihomo
 
 #argon theme
 rm -rf feeds/luci/themes/luci-theme-argon
