f2ba8e4d857f8ce088879308845fce9a20e6a151
Shiori <106437447+Shi0ri00@users.noreply.github.com>
2024-08-21 22:03:13 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -14,8 +14,7 @@ rm -rf feeds/luci/applications/luci-app-passwall
 # Add packages
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth=1 https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
-git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo Openwrt-mihomo
-
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 #argon theme
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
