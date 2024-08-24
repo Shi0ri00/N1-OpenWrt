1548667af8f6a20f0283ab3ce23a0180d1b5bc96
Shiori <106437447+Shi0ri00@users.noreply.github.com>
2024-08-24 16:50:23 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -6,13 +6,14 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 # Default IP
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 
-rm -rf package/custom; mkdir package/custom
+rm -rf package/custom; mkdir 
 
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-passwall
 
 # Add packages
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo package/custom
 #argon theme
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
